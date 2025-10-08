cb44bf62a3af939b9a3908db535e52afc360cc77
Jommp <jommp.dev@gmail.com>
2025-10-08 12:28:34 -0500

    feat(04-hooks-app/scrambleWordsReducer): create SKIP_WORD and START_NEW_GAME actions

--- 04-hooks-app/src/useReducer/reducer/scrambleWordsReducer.tsx
+++ 04-hooks-app/src/useReducer/reducer/scrambleWordsReducer.tsx
@@ -13,7 +13,11 @@ export interface ScrambleWordsState {
   totalWords: number;
 }
 
-export type ScrambleWordsActions = | { type: 'SET_GUESS', payload: string } | { type: 'CHECK_ANSWER' };
+export type ScrambleWordsActions =
+  | { type: 'SET_GUESS', payload: string }
+  | { type: 'CHECK_ANSWER' }
+  | { type: 'SKIP_WORD' }
+  | { type: 'START_NEW_GAME', payload: ScrambleWordsState };
 
 const GAME_WORDS = [
   'REACT',
@@ -89,7 +93,7 @@ export const scrambleWordsReducer = (
           words: updatedWords,
           currentWord: updatedWords[0],
           scrambledWord: scrambleWord(updatedWords[0])
-        }
+        };
       }
 
       return {
@@ -97,8 +101,25 @@ export const scrambleWordsReducer = (
         guess: '',
         errorCounter: state.errorCounter + 1,
         isGameOver: state.errorCounter + 1 >= state.maxAllowErrors
-      }
+      };
     }
+
+    case 'SKIP_WORD': {
+      if (state.skipCounter >= state.maxSkips) return state;
+
+      const updatedWords = state.words.slice(1);
+
+      return {
+        ...state,
+        skipCounter: state.skipCounter + 1,
+        words: updatedWords,
+        currentWord: updatedWords[0],
+        scrambledWord: scrambleWord(updatedWords[0])
+      };
+    }
+
+    case 'START_NEW_GAME':
+      return action.payload;
 
     default:
       return state;

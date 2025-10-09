bd9af188d10df6676924854c13dad12f3cbd210f
Jommp <jommp.dev@gmail.com>
2025-10-08 20:17:22 -0500

    feat(04-hooks-app/ScrambleWords): implement useEffect and confetti when player make a point

--- 04-hooks-app/src/useReducer/ScrambleWords.tsx
+++ 04-hooks-app/src/useReducer/ScrambleWords.tsx
@@ -2,11 +2,14 @@
 // Es necesario componentes de Shadcn/ui
 // https://ui.shadcn.com/docs/installation/vite
 
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Card, CardContent } from '@/components/ui/card';
 import { SkipForward, Play } from 'lucide-react';
+import confetti from 'canvas-confetti';
+
 import { getInitialState, scrambleWordsReducer } from './reducer/scrambleWordsReducer';
 
 export const ScrambleWords = () => {
@@ -25,6 +28,16 @@ export const ScrambleWords = () => {
     words,
     totalWords
   } = state;
+
+  useEffect(() => {
+    if (points === 0) {
+      confetti({
+        particleCount: 100,
+        spread: 120,
+        origin: { y: 0.6 }
+      });
+    }
+  }, [points]);  
 
   const handleGuessSubmit = (e: React.FormEvent) => {
     e.preventDefault();

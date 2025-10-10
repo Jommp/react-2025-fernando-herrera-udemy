813113b7fa0f44d141a7d9514266aee69805bb5c
Jommp <jommp.dev@gmail.com>
2025-10-10 11:54:45 -0500

    feat(04-hooks-app): files for useOptimistic lesson are prepared

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -12,7 +12,9 @@ import './index.css';
 // import { TasksApp } from './05-useReducer/TaskApp';
 // import { ScrambleWords } from './05-useReducer/ScrambleWords';
 // import { MemoHook } from './06-memos/MemoHook';
-import { MemoCounter } from './06-memos/MemoCounter';
+// import { MemoCounter } from './06-memos/MemoCounter';
+
+import { InstagromApp } from './07-useOptimistic/InstragromApp';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -25,7 +27,8 @@ createRoot(document.getElementById('root')!).render(
     {/* <TasksApp /> */}
     {/* <ScrambleWords /> */}
     {/* <MemoHook /> */}
-    
-    <MemoCounter />
+    {/* <MemoCounter /> */}
+
+    <InstagromApp />
   </StrictMode>,
 );

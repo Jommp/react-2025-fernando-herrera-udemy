f38ab8439a7672cc9a8b24ca46d81ed8ac1c6e79
Jommp <jommp.dev@gmail.com>
2025-10-10 11:41:06 -0500

    feat(04-hooks-app/main): update imports

--- 04-hooks-app/src/main.tsx
+++ 04-hooks-app/src/main.tsx
@@ -4,15 +4,15 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 
 // import { HooksApp } from './HooksApp';
-// import { TrafficLight } from './useState/TrafficLight';
-// import { TrafficLightWithEffect } from './useEffect/TrafficLightWithEffect';
-// import { TrafficLightWithHook } from './useEffect/TrafficLightWithHook';
-// import { PokemonPage } from './examples/PokemonPage';
-// import { FocusScreen } from './useRef/FocusScreen';
-// import { TasksApp } from './useReducer/TaskApp';
-// import { ScrambleWords } from './useReducer/ScrambleWords';
-// import { MemoHook } from './useMemo/MemoHook';
-import { MemoCounter } from './useMemo/MemoCounter';
+// import { TrafficLight } from './01-useState/TrafficLight';
+// import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect';
+// import { TrafficLightWithHook } from './02-useEffect/TrafficLightWithHook';
+// import { PokemonPage } from './03-examples/PokemonPage';
+// import { FocusScreen } from './04-useRef/FocusScreen';
+// import { TasksApp } from './05-useReducer/TaskApp';
+// import { ScrambleWords } from './05-useReducer/ScrambleWords';
+// import { MemoHook } from './06-memos/MemoHook';
+import { MemoCounter } from './06-memos/MemoCounter';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

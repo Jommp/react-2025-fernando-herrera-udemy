84a7a800b7a4611126762121fe6a6f0c4bc4ee24
Jommp <jommp.dev@gmail.com>
2025-10-21 12:57:39 -0500

    feat(05-heroes-app/actions): install and configure tanstack react-query

--- 05-heroes-app/src/HeroesApp.tsx
+++ 05-heroes-app/src/HeroesApp.tsx
@@ -1,10 +1,19 @@
 import { RouterProvider } from 'react-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { appRoutes } from './router/app.routes';
+
+const queryClient = new QueryClient()
 
 export const HeroesApp = () => {
   return (
     <>
-      <RouterProvider router={appRoutes} />
+      <QueryClientProvider client={ queryClient }>
+        <RouterProvider router={ appRoutes } />
+
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 };

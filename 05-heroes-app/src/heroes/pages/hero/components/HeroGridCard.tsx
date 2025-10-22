732f8607ab8c938651cfd2deceefe4df5d8b2884
Jommp <jommp.dev@gmail.com>
2025-10-22 16:51:13 -0500

    feat(05-heroes-app/HeroGrid): improve tailwind classes

--- 05-heroes-app/src/heroes/pages/hero/components/HeroGridCard.tsx
+++ 05-heroes-app/src/heroes/pages/hero/components/HeroGridCard.tsx
@@ -14,11 +14,11 @@ interface Props {
 export const HeroGridCard = ({ hero }: Props) => {
   return (
     <Card className="group overflow-hidden hover:shadow-xl transition-all duration-300 hover:-translate-y-1 bg-linear-to-br from-white to-gray-50">
-      <div className="relative h-64 overflow-hidden">
+      <div className="relative h-64">
         <img
           src={ hero.image }
           alt={ hero.name }
-          className="object-cover transition-all duration-500 group-hover:scale-110"
+          className="object-cover transition-all duration-500 group-hover:scale-110 absolute top-[-30px] w-full h-[410px]"
         />
 
         {/* Status indicator */}
@@ -49,7 +49,7 @@ export const HeroGridCard = ({ hero }: Props) => {
         </Button>
       </div>
 
-      <CardHeader className="pb-3">
+      <CardHeader className="py-3 z-10 bg-gray-100/50 backdrop-blur-sm relative top-1 group-hover:-top-2.5 transition-all duration-300">
         <div className="flex justify-between items-start">
           <div className="space-y-1">
             <h3 className="font-bold text-lg leading-tight">

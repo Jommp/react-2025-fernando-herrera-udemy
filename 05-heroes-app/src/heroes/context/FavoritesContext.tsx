b023735dbe93f254134799fb732c23b57d4a9c3a
Jommp <jommp.dev@gmail.com>
2025-10-27 12:08:25 -0500

    feat(05-heroes-app/context/FavoritesContext): implement localStorage set and get items

--- 05-heroes-app/src/heroes/context/FavoritesContext.tsx
+++ 05-heroes-app/src/heroes/context/FavoritesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState, type PropsWithChildren } from 'react';
+import { createContext, useEffect, useState, type PropsWithChildren } from 'react';
 import type { Hero } from '../types/hero.interface';
 
 interface FavoritesContext {
@@ -13,8 +13,19 @@ interface FavoritesContext {
 
 export const FavoritesContext = createContext({} as FavoritesContext);
 
+const getFavoritesFromLocalStorage = (): Hero[] => {
+  const favorites = localStorage.getItem('favorites');
+
+  return favorites ? JSON.parse(favorites) :  [];
+};
+
 export const FavoritesContextProvider = ({ children }: PropsWithChildren) => {
-  const [favorites, setFavorites] = useState<Hero[]>([]);
+  const [favorites, setFavorites] = useState<Hero[]>(getFavoritesFromLocalStorage);
+
+  useEffect(() => {
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }, [favorites])
+  
 
   const isFavorite = (hero: Hero) => {
     return favorites.some((h) => h.id === hero.id);

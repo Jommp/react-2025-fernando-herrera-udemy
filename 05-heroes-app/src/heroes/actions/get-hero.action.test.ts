c66f068c38ae5d8d9913dd1dc2bc9b175be5e016
Jommp <jommp.dev@gmail.com>
2025-11-03 17:28:53 -0500

    feat(05-heroes-app/heroes/actions): create get-hero.action tests

---
+++ 05-heroes-app/src/heroes/actions/get-hero.action.test.ts
@@ -0,0 +1,47 @@
+import { describe, expect, test } from 'vitest';
+import { getHeroAction } from './get-hero.action';
+
+describe('getHeroAction', () => {
+  test('should fetch hero data and return with complete image url', async () => {
+    const idSlug = 'peter-parker';
+    const result = await getHeroAction(idSlug);
+
+    expect(result).toStrictEqual({
+      id: '5',
+      name: 'Peter Parker',
+      slug: 'peter-parker',
+      alias: 'Spider-Man',
+      powers: [
+        'Escalar muros',
+        'Sentido arácnido',
+        'Lanzar telarañas',
+        'Agilidad sobrehumana',
+        'Reflejos mejorados'
+      ],
+      description: 'Tu amistoso vecino Spider-Man, con gran poder viene una gran responsabilidad.',
+      strength: 7,
+      intelligence: 9,
+      speed: 7,
+      durability: 7,
+      team: 'Vengadores',
+      image: 'http://localhost:3001/images/5.jpeg',
+      firstAppearance: '1962',
+      status: 'Active',
+      category: 'Hero',
+      universe: 'Marvel'
+    });
+
+    expect(result.image).toContain('http');
+  });
+
+  test('should throw and error if hero is not found', async () => {
+    const slug = 'spiderman-2';
+
+    const result = await getHeroAction(slug).catch(error => {
+      expect(error).toBeDefined();
+      expect(error.message).toBe('Request failed with status code 404');
+    });
+
+    expect(result).toBeUndefined();
+  });
+});

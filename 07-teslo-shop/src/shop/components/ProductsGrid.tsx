fe175f994b75e13cc52b5e9071a6596d8ef87da9
Jommp <jommp.dev@gmail.com>
2025-11-13 20:32:24 -0500

    feat(06-teslo-shop/shop/components/ProductsGrid): handle viewMode by params

--- 07-teslo-shop/src/shop/components/ProductsGrid.tsx
+++ 07-teslo-shop/src/shop/components/ProductsGrid.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import { useSearchParams } from 'react-router';
 import { Filter, Grid, List } from 'lucide-react';
 
 import type { Product } from '../../mocks/products.mock';
@@ -12,20 +14,37 @@ interface Props {
 }
 
 export const ProductsGrid = ({ products }: Props) => {
+  const [showFilters, setShowFilters] = useState(false);
+  const [ searchParams, setSearchParams ] = useSearchParams();
+
+  const viewMode = searchParams.get('viewMode') || 'grid';
+
+  const handleViewModeChange = ( mode: 'grid' | 'list') => {
+    setSearchParams(prev => {
+      prev.set('viewMode', mode);
+                  
+      return prev;
+    });
+  };
+
   return (
     <section className="py-12 px-4 lg:px-8">
       <div className="container mx-auto">
         <div className="flex items-center justify-between mb-8">
           <div className="flex items-center space-x-4">
-            <h2 className="text-3xl font-light">Productos</h2>
-            <span className="text-muted-foreground">({products.length} productos)</span>
+            <h2 className="text-3xl font-light">
+              Productos
+            </h2>
+            <span className="text-muted-foreground">
+              ({products.length} productos)
+            </span>
           </div>
 
           <div className="flex items-center space-x-2">
             <Button
               variant="outline"
               size="sm"
-              // onClick={() => setShowFilters(!showFilters)}
+              onClick={() => setShowFilters(!showFilters)}
               className="lg:hidden"
             >
               <Filter className="h-4 w-4 mr-2" />
@@ -34,17 +53,17 @@ export const ProductsGrid = ({ products }: Props) => {
 
             <div className="hidden md:flex border rounded-md">
               <Button
-                // variant={viewMode === 'grid' ? 'default' : 'ghost'}
+                variant={viewMode === 'grid' ? 'default' : 'ghost'}
                 size="sm"
-                // onClick={() => setViewMode('grid')}
+                onClick={() => handleViewModeChange('grid')}
                 className="rounded-r-none"
               >
                 <Grid className="h-4 w-4" />
               </Button>
               <Button
-                // variant={viewMode === 'list' ? 'default' : 'ghost'}
+                variant={viewMode === 'list' ? 'default' : 'ghost'}
                 size="sm"
-                // onClick={() => setViewMode('list')}
+                onClick={() => handleViewModeChange('list')}
                 className="rounded-l-none"
               >
                 <List className="h-4 w-4" />
@@ -60,7 +79,7 @@ export const ProductsGrid = ({ products }: Props) => {
           </div>
 
           {/* Mobile Filters */}
-          {/* {showFilters && (
+          {showFilters && (
             <div className="fixed inset-0 z-50 bg-background p-4 lg:hidden">
               <div className="flex items-center justify-between mb-6">
                 <h3 className="text-lg font-semibold">Filtros</h3>
@@ -74,16 +93,16 @@ export const ProductsGrid = ({ products }: Props) => {
               </div>
               <FilterSidebar />
             </div>
-          )} */}
+          )}
 
           {/* Products Grid */}
           <div className="flex-1">
-            {/* <div className={
+            <div className={
               viewMode === 'grid'
                 ? "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
                 : "space-y-4"
             }>
-              {currentProducts.map((product) => (
+              {products.map((product) => (
                 <ProductCard
                   key={product.id}
                   id={product.id}
@@ -93,7 +112,7 @@ export const ProductsGrid = ({ products }: Props) => {
                   category={product.category}
                 />
               ))}
-            </div> */}
+            </div>
           </div>
         </div>
       </div>

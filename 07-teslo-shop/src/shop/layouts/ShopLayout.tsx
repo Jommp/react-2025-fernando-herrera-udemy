45f2b3c429462c9c74251548e67e0d99d96517eb
Jommp <jommp.dev@gmail.com>
2025-11-13 19:40:18 -0500

    feat(06-teslo-shop/shop): implement CustomHeader and CustomFooter in ShopLayout

--- 07-teslo-shop/src/shop/layouts/ShopLayout.tsx
+++ 07-teslo-shop/src/shop/layouts/ShopLayout.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router';
 
+import { CustomHeader } from '../components/CustomHeader';
+import { CustomFooter } from '../components/CustomFooter';
+
 export const ShopLayout = () => {
   return (
-    <div>
-      <h1>ShopLayout</h1>
+    <div className="min-h-screen bg-background">
+      <CustomHeader />
 
       <Outlet />
+
+      <CustomFooter />
     </div>
   );
 };

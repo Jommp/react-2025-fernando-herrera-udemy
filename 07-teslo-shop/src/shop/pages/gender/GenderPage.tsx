2ba6903a5485b045e22cc4d1b4abb2bdb357bc64
Jommp <jommp.dev@gmail.com>
2025-11-19 17:43:53 -0500

    feat(06-teslo-shop/shop/pages/GenderPage): implement components to display products and page sections

--- 07-teslo-shop/src/shop/pages/gender/GenderPage.tsx
+++ 07-teslo-shop/src/shop/pages/gender/GenderPage.tsx
@@ -1,6 +1,39 @@
+import { useParams } from 'react-router';
+
+import { products } from '@/mocks/products.mock';
+
+import { CustomPagination } from '@/components/custom/CustomPagination';
+import { CustomJumbotron } from '@/shop/components/CustomJumbotron';
+import { ProductsGrid } from '@/shop/components/ProductsGrid';
+
+const genders = {
+  'men': 'Hombre',
+  'women': 'Mujer',
+  'kids': 'Niños'
+};
+
+type GenderKey = keyof typeof genders;
 
 export const GenderPage = () => {
+  const { gender } = useParams();
+
+  const defaultGender: GenderKey = 'men';
+
+  const genderIndex = (gender && gender in genders)
+    ? (gender as GenderKey)
+    : defaultGender;
+
+  const genderLabel = genders[genderIndex];
+
+  const pageTitle = `Productos para ${genderLabel}`
+
   return (
-    <div>GenderPage</div>
+    <>
+      <CustomJumbotron title={pageTitle} />
+      
+      <ProductsGrid products={products} />
+
+      <CustomPagination totalPages={6} />
+    </>
   );
 };
